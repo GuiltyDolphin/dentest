930f284fc8e726290c826afe7d670c21f66b0ba5
Ben Moon <software@guiltydolphin.com>
2021-05-24 09:33:56 +0100

    Re-export Deno's assertions

--- mod.ts
+++ mod.ts
@@ -7,3 +7,18 @@ export {
 export type {
     Testable
 } from './src/dentest.ts';
+
+export {
+    assert,
+    assertEquals,
+    assertExists,
+    assertNotEquals,
+    assertStrictEquals,
+    assertStringIncludes,
+    assertArrayIncludes,
+    assertMatch,
+    assertNotMatch,
+    assertObjectMatch,
+    assertThrows,
+    assertThrowsAsync,
+} from 'https://deno.land/std@0.97.0/testing/asserts.ts';
