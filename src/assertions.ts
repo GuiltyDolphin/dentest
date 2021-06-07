9eac7ad6d16ca1d3d0b840304d4634b174fd869a
Ben Moon <software@guiltydolphin.com>
2021-06-07 16:34:56 +0100

    Add empty Assertion interface

--- src/assertions.ts
+++ src/assertions.ts
@@ -1,52 +1,54 @@
 import * as da from 'https://deno.land/std@0.97.0/testing/asserts.ts';
 
+export interface Assertion { }
+
 /** Assert that the expression is true. */
-export function assert(expr: boolean, msg = ""): void {
+export function assert(expr: boolean, msg = ""): Assertion {
     return da.assert(expr, msg);
 }
 
 /** Assert that the two expressions are deeply equal. */
-export function assertEquals<T>(actual: T, expected: T, msg?: string): void {
+export function assertEquals<T>(actual: T, expected: T, msg?: string): Assertion {
     return da.assertEquals(actual, expected, msg);
 }
 
 /** Assert that the two expressions are not deeply equal. */
-export function assertNotEquals<T>(actual: T, expected: T, msg?: string): void {
+export function assertNotEquals<T>(actual: T, expected: T, msg?: string): Assertion {
     return da.assertNotEquals(actual, expected, msg);
 }
 
 /** Assert that the two expressions are strictly equal. */
-export function assertStrictEquals<T>(actual: T, expected: T, msg?: string): void {
+export function assertStrictEquals<T>(actual: T, expected: T, msg?: string): Assertion {
     return da.assertStrictEquals(actual, expected, msg);
 }
 
 /** Assert that the expression is not `null` or `undefined` */
-export function assertExists<T>(actual: T, msg?: string): void {
+export function assertExists<T>(actual: T, msg?: string): Assertion {
     return da.assertExists(actual, msg);
 }
 
 /** Assert that the second string is included in the first. */
-export function assertStringIncludes(actual: string, expected: string, msg?: string): void {
+export function assertStringIncludes(actual: string, expected: string, msg?: string): Assertion {
     return da.assertStringIncludes(actual, expected, msg);
 }
 
 /** Assert that the second array is a sub-array of the first. */
-export function assertArrayIncludes<T>(actual: T[], expected: T[], msg?: string): void {
+export function assertArrayIncludes<T>(actual: T[], expected: T[], msg?: string): Assertion {
     return da.assertArrayIncludes(actual, expected, msg);
 }
 
 /** Assert that the string matches the given regular expression. */
-export function assertMatch(actual: string, expected: RegExp, msg?: string): void {
+export function assertMatch(actual: string, expected: RegExp, msg?: string): Assertion {
     return da.assertMatch(actual, expected, msg);
 }
 
 /** Assert that the string does not match the given regular expression. */
-export function assertNotMatch(actual: string, expected: RegExp, msg?: string): void {
+export function assertNotMatch(actual: string, expected: RegExp, msg?: string): Assertion {
     return da.assertNotMatch(actual, expected, msg);
 }
 
 /** Assert that the second object is (deeply) a sub-object of the first object. */
-export function assertObjectMatch(actual: Record<PropertyKey, unknown>, expected: Record<PropertyKey, unknown>): void {
+export function assertObjectMatch(actual: Record<PropertyKey, unknown>, expected: Record<PropertyKey, unknown>): Assertion {
     return da.assertObjectMatch(actual, expected);
 }
 
